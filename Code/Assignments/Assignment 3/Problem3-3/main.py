81750593bfe27c1d73383b0a18c1bbe712ae2331
Tatsu <sweetta@uw.edu>
2020-04-29 00:57:36 -0700

    Added Plotting for Problem 3

--- Code/Assignments/Assignment 3/Problem3-3/main.py
+++ Code/Assignments/Assignment 3/Problem3-3/main.py
@@ -17,12 +17,12 @@ dH = H/11   # Story Height [m]
 EA1 = 2000  # Vertical Members [kN]
 EA2 = 5000  # Horizontal Ties [kN]
 EA3 = 5000  # Diagonals [kN]
-Pref = 119.5    # Reference Load [kN]
+Pref = 6.1685    # Reference Load [kN]
 
 # Convergence criteria
 a = 0.0
 max_iteration = 50
-tol = 1e-10     # np.linalg.norm([R],[g]) <= tol
+tol = 5e-11     # np.linalg.norm([R],[g]) <= tol
 
 # Create Nodes
 X = []
@@ -32,11 +32,11 @@ for idx in range(12):
     X.append(xl)
     X.append(xr)
 
-fixed_dof = [0, 1, 2, 3]
-free_dof = range(4, 48)
-
 dim = len(X[0])
 ndof = dim*len(X)
+
+fixed_dof = [0, 1, 2, 3]
+free_dof = range(4, ndof)
 
 # Create Trusses
 T = []
@@ -54,13 +54,16 @@ P0   = np.zeros(ndof)
 Pbar = np.zeros(ndof)
 Pbar[dim*22+1] = -Pref/2
 Pbar[dim*23+1] = -Pref/2
+# Pbar[dim*22] = Pref/20
+# Pbar[dim*23+1] = Pref/20
+
 P0 = P0[free_dof]
 Pbar = Pbar[free_dof]
 
 # Start at u = zero, gamma = 0.2
 u_n = np.zeros(ndof)
 gamma_n = 0.0
-gamma_guess = 0.2
+gamma_guess = 1
 
 # Initialization Lists for plotting
 zeroU = np.zeros(ndof)
@@ -71,12 +74,10 @@ Uu = [0]
 Uv = [0]
 G = [0]
 
-# Initial Step to find s2
-
+# Initial Step to find delta_s^2
 # Set Displacements and Construct Global K Matrix
 K_global = np.zeros([ndof, ndof])
 Fint = np.zeros(ndof)
-
 for ele in T:
     xi = ele.nodeID[0]
     xj = ele.nodeID[1]
@@ -102,8 +103,7 @@ s2 = np.dot(u_guess[free_dof], u_guess[free_dof]) + a*gamma_guess*gamma_guess
 print('(delta_s)^2 = {}, delta_s = {}'.format(s2, np.sqrt(s2)))
 
 flag = 0
-for i in range(105):
-
+for i in range(500):
     for count in range(max_iteration + 1):
         K_global = 0*K_global
         Fint = 0*Fint
@@ -121,40 +121,48 @@ for i in range(105):
             Fint[xi_idx] -= ele.get_F()
             Fint[xj_idx] += ele.get_F()
 
+        # Construct Kg Matrix
         Kf = K_global[free_dof, :][:, free_dof]
-        Fintf = Fint[free_dof]
-
         Kg = np.hstack((Kf, np.array([-Pbar]).T))
         Kg = np.vstack((Kg, np.append(-2*(u_guess-u_n)[free_dof], -2*a*(gamma_guess-gamma_n))))
 
+        # Construct Rg Vector
         P = P0 + gamma_guess * Pbar
-        R = P - Fintf
-
+        R = P - Fint[free_dof]
         g = np.dot((u_guess-u_n)[free_dof], (u_guess-u_n)[free_dof]) + a*(gamma_guess-gamma_n)*(gamma_guess-gamma_n) - s2
         Rg = np.append([R], [g])
 
+        # Solve for Change in u and gamma guess
         dU = np.dot(np.linalg.inv(Kg), Rg)
 
+        # Update Guess
         u_guess[free_dof] = u_guess[free_dof] + dU[0:len(free_dof)]
         gamma_guess = gamma_guess + dU[-1]
 
+        # Check Convergence
         if np.linalg.norm(Rg) <= tol:
-            print('u = {}m, P = {:.9f} kN = {}Pbar, '
+            print('u = {}m, P = {:.6f} kN = {:.6f} Pbar, '
                   'Iteration Count = {}, u_{}'.format(u_n, gamma_n*Pref, gamma_n, count, i))
             break
 
         # Save and Print Results if it does not converge by max iteration
         if count == max_iteration:  # Exit loop if max iteration is reached before convergence
-            print('u = {}m, P = {} kN = {}Pbar, '
+            print('u = {}m, P = {:.6f} kN = {:.6f} Pbar, '
                   'Stopped at Max Iteration = {}'.format(u_guess, gamma_n*Pref, gamma_guess, count))
             flag = 1
+
+        # Exit if top node hits the ground
+        if u_guess[-1] + X[23][1] <= -0.05:
+            flag = 1
+
     if flag == 1:
         break
 
+    # Store Converged Results
     u_list = np.vstack((u_list, u_guess))
-    Uu.append(u_guess[46])
-    Uv.append(u_guess[47])
     G.append(gamma_guess)
+
+    # Initiate Next Guess
     u_last = u_n
     u_n = u_guess
     u_guess = 2*u_n - u_last
@@ -162,24 +170,34 @@ for i in range(105):
     gamma_n = gamma_guess
     gamma_guess = 2*gamma_n - gamma_last
 
+# Transpose for easier plotting
 u_list = u_list.transpose()
+
 # ----------------------------------------------------------------------------------------------------------------------
-# Plot of gamma vs displacement, part 3 of assignment problem
+# Plot of Deformations
 # ----------------------------------------------------------------------------------------------------------------------
 plt.figure(1, figsize=(8, 8))
 
-
+# Plotting Node Trajectory
 for i in range(24):
-    plt.plot(u_list[2*i] + X[i][0], u_list[2*i+1] + X[i][1], label="$U_u$", marker='.')
-# plt.plot(u_list[44] + X[22][0], u_list[45] + X[22][1], label="$U_u$", marker='.')
-# plt.plot(u_list[46] + X[23][0], u_list[47] + X[23][1], label="$U_u$", marker='.')
+    plt.plot(u_list[2*i] + X[i][0], u_list[2*i+1] + X[i][1], label="Node {}".format(i), linestyle='--')
+
+# Plotting Final Deformed Shape
+for ele in T:
+    xi = ele.nodeID[0]
+    xj = ele.nodeID[1]
+    xi_idx = range(xi * dim, (xi + 1) * dim)
+    xj_idx = range(xj * dim, (xj + 1) * dim)
+    qi = u_list[xi_idx, -1] + X[xi]
+    qj = u_list[xj_idx, -1] + X[xj]
+    plt.plot([qi[0], qj[0]], [qi[1], qj[1]], color='black', marker='.')
 
 plt.xlabel('u [m]')
 plt.ylabel('v [m]')
 
-# plt.legend(loc='best', ncol=1, framealpha=1)
 plt.axis('equal')
 plt.grid(True)
+# plt.savefig("CESG506/Code/Assignments/Assignment 3/Problem3-3/Prob3-3_DeformedShape")
 plt.show()
 
 # # ----------------------------------------------------------------------------------------------------------------------
